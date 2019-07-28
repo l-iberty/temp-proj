cf091f28b3e08e2c371d783be79fefd23734cd6b
l-iberty <2593093242@qq.com>
2019-07-28 16:40:37 +0800

    ...

--- Main.java
+++ Main.java
@@ -1,29 +1,55 @@
 import java.util.*;
 
 public class Main {
-    boolean solve2(String[] args) {
-        int[] stat = new int[26];
-        for (String arg : args) {
-            stat[arg.charAt(0) - 'A']++;
-            stat[arg.charAt(arg.length() - 1) - 'A']++;
+    void solve1(int[] A, int[] B) {
+        int i, j;
+        for (i = 1; i < A.length; i++) {
+            if (A[i] < A[i - 1]) break;
         }
-        for (int i = 0; i < stat.length; i++) {
-            if (stat[i] % 2 != 0) return false;
+
+        Queue<Integer> q = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        for (int x : B) q.add(x);
+
+        int x = Integer.MAX_VALUE;
+        while (!q.isEmpty() && i + 1 < A.length) {
+            x = q.poll();
+            if (x < A[i + 1] && x > A[i - 1]) break;
         }
-        return true;
+
+        if (x == Integer.MAX_VALUE) {
+            System.out.println("NO");
+        } else {
+            A[i] = x;
+            for (j = 0; j < A.length - 1; j++) {
+                System.out.print(A[j] + " ");
+            }
+            if (j < A.length) {
+                System.out.println(A[j]);
+            }
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Main m = new Main();
         while (sc.hasNext()) {
-            String line = sc.nextLine();
-            StringTokenizer tokenizer = new StringTokenizer(line, " ");
-            String[] strs = new String[tokenizer.countTokens()];
-            for (int i = 0; i < strs.length && tokenizer.hasMoreTokens(); i++) {
-                strs[i] = tokenizer.nextToken();
+            String[] strA = sc.nextLine().split(" ");
+            String[] strB = sc.nextLine().split(" ");
+            int[] A = new int[strA.length];
+            int[] B = new int[strB.length];
+            for (int i = 0; i < strA.length; i++) {
+                A[i] = Integer.valueOf(strA[i]);
             }
-            System.out.println(m.solve2(strs));
+            for (int i = 0; i < strB.length; i++) {
+                B[i] = Integer.valueOf(strB[i]);
+            }
+            m.solve1(A, B);
         }
+
     }
 }
